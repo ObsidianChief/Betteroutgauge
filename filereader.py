c3d1ab97e4a58308dd8e3d87aa3ba955f890633c
ObsidianChief <obsidianchief@gmail.com>
2019-09-08 21:49:59 -0700

    filereader updated to read raw binary from the file Spoofer, printer updated to print raw binary to the Spoofer file

---
+++ filereader.py
@@ -0,0 +1,17 @@
+import struct
+import socket
+import time
+#import bytesio
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+ip = "127.0.0.1"
+port = 4444
+#n = 1
+#b = Bytesio()
+f = open("Spoofer", "br")
+while True:
+    data = f.read(92)
+    time.sleep(0.03)
+    #print(data)
+    #i = data.encode('utf-8','ignore')
+    print(data)
+    s.sendto(data,(ip,port))
--- printer.py
+++ printer.py
@@ -1,7 +1,8 @@
 import socket
+import struct
 #import sys
 #import time
-pt = open("Spoofer.txt", "a+")
+pt = open("Spoofer", "br+")
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(("127.0.0.1", 4444))
 if True:
@@ -9,6 +10,12 @@ if True:
     while True:
         data, addr=s.recvfrom(92)
         if data:
-            datastr = str(data)
-            pt.writelines(datastr)
-            pt.write("\n")
+            Time, car, flags, gear, plid, speed, rpm, turbo, engTemp, fuel, oilPressure, oilTemp, dashLights, showLights, throttle, brake, clutch, display1, display2, id =  struct.unpack('Bxxx4sHssfffffffBBfff16s16si', data)
+            pt.write(struct.pack('Bxxx4sHssfffffffBBfff16s16si', Time, car, flags, gear, plid, speed, rpm, turbo, engTemp, fuel, oilPressure, oilTemp, dashLights, showLights, throttle, brake, clutch, display1, display2, id))
+            
+            
+            
+            #pt.write(data)
+            #datastr = str(data)
+            #pt.writelines(datastr)
+            #pt.write("\n")
