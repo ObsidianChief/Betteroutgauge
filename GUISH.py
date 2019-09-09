078501dc28953bd8524955ba5ab126b8957cfaad
ObsidianChief <obsidianchief@gmail.com>
2019-09-09 00:07:59 -0700

    pls work

--- GUISH.py
+++ GUISH.py
@@ -1,15 +1,35 @@
+import kivy
+kivy.require('1.1.0')
+
+
 from kivy.app import App
-from kivy.uix.widget import Widget
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.properties import ObjectProperty, StringProperty
+from kivy.uix.popup import Popup
 
 
-class PongGame(Widget):
+#class RPM(GridLayout):
+
+
+class MainMenu(GridLayout):
+    info = StringProperty()
+
+    def do_action(self):
+        self.info.text = range(10000)
+
+class HiddenPopUp(Popup):
     pass
 
 
-class PongApp(App):
+class Display(App):
+    title = "test of the emergency alert system"
+    
     def build(self):
-        return PongGame()
+        return MainMenu()
+
 
 
 if __name__ == '__main__':
-    PongApp().run()
+    Display().run()  
